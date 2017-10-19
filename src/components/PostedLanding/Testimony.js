53bd1f3cf898ba2413fae64bed9402c6db9e1e0d
Sharath Vignesh <sharathvignesh08@gmail.com>
2017-10-19 23:15:36 +0900

    testimonial done

--- src/components/PostedLanding/Testimony.js
+++ src/components/PostedLanding/Testimony.js
@@ -9,8 +9,37 @@ class Testimony extends Component {
           <h1>CUSTOMER TESTIMONALS</h1>
           <p>Serving happy customers globally, Infotech believe in
           building relationships, not just partnerships. They say it. We claim it.</p>
-          
            <br />
+           <div className='testimony-card'>
+            <div className='testimony-client-details'>
+             <img src={require('./testi/1.png')} alt='logo' />
+               <h3>Ammenda Michle</h3>
+               <h5>Gaming Apps Consultant at AppManias</h5>
+               <h5>An iOS App Development Project</h5>
+              </div>
+              <div>
+                <i className="fa fa-quote-left" aria-hidden="true"></i>
+                 <p>
+                    Great service, professional team members, and courteous sales representatives.
+                     Loved their work and will definitely use Infotech’s services again.
+                  </p>
+              </div>
+           </div>
+           <div className='testimony-card'>
+             <div className='testimony-client-details'>
+              <img src={require('./testi/2.jpeg')} alt='logo' />
+                <h3>Ammenda Michle</h3>
+                <h5>Gaming Apps Consultant at AppManias</h5>
+                <h5>An iOS App Development Project</h5>
+               </div>
+               <div>
+               <i className="fa fa-quote-left" aria-hidden="true"></i>
+                 <p>
+                    Great service, professional team members, and courteous sales representatives.
+                     Loved their work and will definitely use Infotech’s services again.
+                  </p>
+                </div>
+           </div>
            <br />
            <hr />
         </div>
