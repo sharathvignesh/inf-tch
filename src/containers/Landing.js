ad02b2428fc6e181c8dbdb197a0da7256a3971ae
Suresh Murali <m.suresh.fx@gmail.com>
2017-11-07 22:09:10 +0900

    Added digital strats

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Style from '../Styles/landing.css';
 import Header from '../components/Header/Header';
+import Strategy from '../components/Landing/Strategy';
 import Tech from '../components/Landing/Technologies';
 import Customers from '../components/Landing/Customers';
 import Testimony from '../components/Landing/Testimony';
@@ -17,7 +18,9 @@ componentDidMount(){
         <Header />
         <Banner />
         <br />
-        <div id='content-wrapper'>
+        <div id='content-wrapper'>      
+        <Strategy />
+        <br /><br />
           <Tech />
           <br /><br />
           <Customers />
