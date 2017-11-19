55094a273a58f9531deaddc20cab8abe345379b2
Suresh Murali <m.suresh.fx@gmail.com>
2017-11-19 21:06:53 +0900

    Salesforce content added

--- src/components/Technologies/Salesforce/Insurance.js
+++ src/components/Technologies/Salesforce/Insurance.js
@@ -4,20 +4,35 @@ class Salesforce extends Component {
 
   render () {
     return (
-        <div id='tech-wrapper'>
-          <h1>Sales force</h1>
+      <div id='tech-wrapper'>
+        <h1>Sales force</h1>
           <p>
-Infotech delivers full-cycle Salesforce consulting services to help you embrace the world’s leading CRM platform in the entirety of its benefits for your business.</p>
-        <p>We have worked with a variety of industries, delivering precise solutions to grow sales and customer engagement in Banking, Retail, Manufacturing, Media, Public Sector and more.</p>
-<p>
-As your Salesforce consulting company, we will help you decide on the most suitable license option according to the scope and demands of your business. You will get the project price estimate immediately after we gather and analyze your requirements.</p>
-          <p>
-
-With in-depth awareness of the platform, we deliver well-grounded Salesforce CRM consulting services to our clients. Reach out to learn how you can benefit from this world’s leading CRM platform
+          Infotech delivers full-cycle Salesforce consulting services to help you embrace the world’s leading CRM platform in the entirety of its benefits for your business.
           </p>
-           <br />
-           <hr />
-        </div>
+          <p>We have worked with a variety of industries, delivering precise solutions to grow sales and customer engagement in Banking, Retail, Manufacturing, Media, Public Sector and more.
+          </p>
+          <p>As your Salesforce consulting company, we will help you decide on the most suitable license option according to the scope and demands of your business. You will get the project price estimate immediately after we gather and analyze your requirements.
+          </p>
+          <p>With in-depth awareness of the platform, we deliver well-grounded Salesforce CRM consulting services to our clients. Reach out to learn how you can benefit from this world’s leading CRM platform
+          </p>
+        <br />
+        <h2>Sales Cloud</h2>
+        <p> What if selling were smarter, and faster? Sales Cloud lets you focus on what matters most: your customer.
+        </p>
+        <br />
+        <h2>Service Cloud</h2>
+        <p> Service Cloud Lightning lets you deliver a new era of service in a hyper-connected world. Deliver smarter, faster one-to-one support to every customer with Service Cloud Lightning
+        </p>
+        <br />
+        <h2>App Cloud</h2>
+        <p> Salesforce App Cloud is a new kind of Platform-as-a-Service - a single ecosystem for building, connecting, and running all of your apps.
+        </p>
+        <br />
+        <h2>Marketing Cloud</h2>
+        <p> Learn how to build 1-to-1 journeys with the Salesforce Marketing Cloud. Unify data, personalize interactions, and connect to customers via email, mobile, social, ads and the web.
+        </p>
+        <hr />
+      </div>
     )
   }
 }
