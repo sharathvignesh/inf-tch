09e38a05cd913681b6383d2b48680be2d8a87d6d
Sharath Vignesh <sharathvignesh08@gmail.com>
2017-12-10 23:10:28 +0900

    link updated in contact page

--- src/components/Contact/Info.js
+++ src/components/Contact/Info.js
@@ -12,7 +12,7 @@ class Info extends Component {
           <div><i className="fa fa-phone" aria-hidden="true"></i><h3>+61 421995678</h3></div><br /><br />
           <i className="fa fa-envelope-o" aria-hidden="true"></i><h3>admin@infotechconsultancy.com.au</h3><br /><br />
           <i className="fa fa-facebook-square" aria-hidden="true"></i><h3>fb.com/infotechconsultancy.com.au</h3><br /><br />
-          <i className="fa fa-linkedin-square" aria-hidden="true"></i><h3>linkedin.com/company/infotech</h3>
+          <i className="fa fa-linkedin-square" aria-hidden="true"></i><h3>https://www.linkedin.com/in/infotech-consultancy-872417155/</h3>
         </div>
     )
   }
